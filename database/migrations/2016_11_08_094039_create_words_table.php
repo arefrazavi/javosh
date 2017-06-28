9db2c37cddfd78705a4e451e49691aba974f25b5
Aref Razavi <arefrazavi@gmail.com>
2017-06-28 12:48:37 +0430

    correct migrations

--- database/migrations/2016_11_08_094039_create_words_table.php
+++ database/migrations/2016_11_08_094039_create_words_table.php
@@ -15,7 +15,7 @@ class CreateWordsTable extends Migration
     {
         Schema::create('words', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('value');
+            $table->string('value')->unique();
             $table->integer('count');
             $table->text('occurrences');
             $table->text('entropy')->nullable();
