b8a1fa5b63cc26c994d3ede64b966d01ced734d6
Aref Razavi <arefrazavi@gmail.com>
2017-06-28 13:36:20 +0430

    correct migrations

--- database/migrations/2017_05_29_120223_add_pos_tag_and_sentiment_polarity_fields_Fields_to_words_table.php
+++ database/migrations/2017_05_29_120223_add_pos_tag_and_sentiment_polarity_fields_Fields_to_words_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddPosTagAndSentimentPolarityToWordsTable extends Migration
+class AddPosTagAndSentimentPolarityFieldsToWordsTable extends Migration
 {
     /**
      * Run the migrations.
