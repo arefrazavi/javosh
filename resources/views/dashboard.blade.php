5dc1695fd0d5722c000fd198bcab69aca139f951
Aref Razavi <arefrazavi@gmail.com>
2017-07-25 14:10:54 +0430

    change category_id

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,7 +17,7 @@
                 <h4 class="text-center">@lang('common_lang.Products_List') @lang('common_lang.Category') @lang('common_lang.Laptop')</h4>
             </a>
 
-            <a class="block alert alert-lucky text-center" href="{{ route("ProductController.viewList", ['categoryId' => 4, 'limit' => 10]) }}">
+            <a class="block alert alert-lucky text-center" href="{{ route("ProductController.viewList", ['categoryId' => 6, 'limit' => 10]) }}">
                 <h5>@lang('common_lang.Summarize_10_lucky_products')</h5>
             </a>
         </div><!-- /.col -->
