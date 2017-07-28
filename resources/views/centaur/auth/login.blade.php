260dc7a00e95d6e28177e68051075eca0c453d46
Aref Razavi <arefrazavi@gmail.com>
2017-07-28 15:26:43 +0430

    add register button

--- resources/views/centaur/auth/login.blade.php
+++ resources/views/centaur/auth/login.blade.php
@@ -3,36 +3,44 @@
 @section('title', trans('user.Login'))
 
 @section('content')
-<div class="row rtl-text">
-    <div class="col-md-4 col-md-offset-4">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="panel-title rtl-text" style="direction: rtl">@lang('common_lang.Login')</h3>
-            </div>
-            <div class="panel-body">
-                <form accept-charset="UTF-8" role="form" method="post" action="{{ route('auth.login.attempt') }}">
-                <fieldset>
-                    <div class="form-group {{ ($errors->has('email')) ? 'has-error' : '' }}">
-                        <input class="form-control" placeholder="@lang('user.email')" name="email" type="text" value="{{ old('email') }}">
-                        {!! ($errors->has('email') ? $errors->first('email', '<p class="text-danger">:message</p>') : '') !!}
-                    </div>
-                    <div class="form-group  {{ ($errors->has('password')) ? 'has-error' : '' }}">
-                        <input class="form-control" placeholder="@lang('user.password')" name="password" type="password" value="">
-                        {!! ($errors->has('password') ? $errors->first('password', '<p class="text-danger">:message</p>') : '') !!}
-                    </div>
-                    <div class="checkbox">
-                        <label>
-                            <input name="remember" type="checkbox" value="true" {{ old('remember') == 'true' ? 'checked' : ''}}>
-                                <span style="padding-right: 20px"> @lang('common_lang.Remember_Me') </span>
-                        </label>
-                    </div>
-                    <input name="_token" value="{{ csrf_token() }}" type="hidden">
-                    <input class="btn btn-lg btn-primary btn-block" type="submit" value="@lang('common_lang.Login')">
-                    <p style="margin-top:5px; margin-bottom:0"><a href="{{ route('auth.password.request.form') }}" type="submit">@lang('common_lang.Forgot_Password')</a></p>
-                </fieldset>
-                </form>
+    <div class="row rtl-text">
+        <div class="col-md-4 col-md-offset-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title rtl-text" style="direction: rtl">@lang('common_lang.Login')</h3>
+                </div>
+                <div class="panel-body">
+                    <form accept-charset="UTF-8" role="form" method="post" action="{{ route('auth.login.attempt') }}">
+                        <fieldset>
+                            <div class="form-group {{ ($errors->has('email')) ? 'has-error' : '' }}">
+                                <input class="form-control" placeholder="@lang('user.email')" name="email" type="text"
+                                       value="{{ old('email') }}">
+                                {!! ($errors->has('email') ? $errors->first('email', '<p class="text-danger">:message</p>') : '') !!}
+                            </div>
+                            <div class="form-group  {{ ($errors->has('password')) ? 'has-error' : '' }}">
+                                <input class="form-control" placeholder="@lang('user.password')" name="password"
+                                       type="password" value="">
+                                {!! ($errors->has('password') ? $errors->first('password', '<p class="text-danger">:message</p>') : '') !!}
+                            </div>
+                            <div class="checkbox">
+                                <label>
+                                    <input name="remember" type="checkbox"
+                                           value="true" {{ old('remember') == 'true' ? 'checked' : ''}}>
+                                    <span style="padding-right: 20px"> @lang('common_lang.Remember_Me') </span>
+                                </label>
+                            </div>
+                            <input name="_token" value="{{ csrf_token() }}" type="hidden">
+                            <input class="btn btn-lg btn-primary btn-block" type="submit"
+                                   value="@lang('common_lang.Login')">
+
+                            <div style="margin-top:6px; margin-bottom:0">
+                                <p><a href="{{ route('auth.register.form') }}">@lang('common_lang.Register')</a></p>
+                                <p><a href="{{ route('auth.password.request.form') }}" type="submit">@lang('common_lang.Forgot_Password')</a></p>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
 @stop
