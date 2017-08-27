aaa5130479b131fed84c121cd30a812382282c56
Aref Razavi <arefrazavi@gmail.com>
2017-08-27 23:16:42 +0430

    auto activation

--- resources/views/centaur/email/welcome.blade.php
+++ resources/views/centaur/email/welcome.blade.php
@@ -8,7 +8,7 @@
         <h2 style="text-align: right">@lang("user.Welcome_To_Javosh")</h2>
         <p style="direction: rtl; text-align: right">@lang("user.Account"): {{ $email }}</p>
         <p style="direction: rtl; text-align: right">@lang("user.login_to_your_account_with_following_link"): </p>
-        <p style="direction: ltr"><a href="{!! route('auth.login.form') !!}">@lang("user.login_link")</a></p>
+        <p><a href="{!! route('auth.login.form') !!}">@lang("user.login_link")</a></p>
         <p style="direction: rtl; text-align: right">@lang("user.Thank_you!")</p>
     </div>
 </body>
