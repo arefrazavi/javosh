6d75ef56ef48f4525edc680caaad86e0ccfcb725
Aref Razavi <arefrazavi@mail.com>
2018-06-24 00:46:23 +0430

    fix width problem in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master-admin')
 @section('title', trans('common_lang.Dashboard'))
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
         <div class='row'>
             <div class='col-md-6 col-sm-6 col-xs-12'>
                 <div>
